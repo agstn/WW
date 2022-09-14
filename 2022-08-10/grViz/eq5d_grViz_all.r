f15bf1046ceee2c1bb8969a7d2797adb5c53d429
Agustin Calatroni <acalatroni@gmail.com>
2022-09-14 10:07:44 -0700

    Add files via upload

--- 2022-08-10/grViz/eq5d_grViz_all.r
+++ 2022-08-10/grViz/eq5d_grViz_all.r
@@ -15,34 +15,28 @@ MARSTA [shape = ellipse, style = filled, fillcolor = '#FED9A6']
 EDU    [shape = ellipse, style = filled, fillcolor = '#CCEBC5']
 NCOND  [shape = ellipse, style = filled, fillcolor = '#FBB4AE', label = '#COND']
 
-INTER1 [shape=point, width=0.05, height=0.05]
-INTER2 [shape=point, width=0.05, height=0.05]
-INTER3 [shape=point, width=0.05, height=0.05]
-INTER4 [shape=point, width=0.05, height=0.05]
-
 NCOND      -> EQ5D [style = bold, arrowsize = 0.7]
 EDU        -> EQ5D [style = bold, arrowsize = 0.7]
 
+INTER1 [shape=point, width=0.05, height=0.05]
 SEX           -> EQ5D   [style = bold, arrowsize = 0.7]
 {SEX, MARSTA} -> INTER1 [style = bold,  dir=none, style = dashed]
 INTER1        -> EQ5D   [style = bold, arrowsize = 0.7, style = dashed]
 MARSTA        -> EQ5D   [style = bold, arrowsize = 0.7]
 
+INTER2 [shape=point, width=0.05, height=0.05]
 AGE          -> EQ5D   [style = bold, arrowsize = 0.7]
 {AGE, BMI}   -> INTER2 [style = bold, dir=none, style = dashed]
 INTER2       -> EQ5D   [style = bold, arrowsize = 0.7, style = dashed]
 BMI          -> EQ5D   [style = bold, arrowsize = 0.7]
 
-AGE          -> EQ5D   [style = bold, arrowsize = 0.7]
+INTER3 [shape=point, width=0.05, height=0.05]
 {AGE, MARSTA}-> INTER3 [style = bold, dir=none, style = dashed]
 INTER3       -> EQ5D   [style = bold, arrowsize = 0.7, style = dashed]
-MARSTA       -> EQ5D   [style = bold,arrowsize = 0.7]
 
-MARSTA       -> EQ5D   [style = bold, arrowsize = 0.7]
+INTER4 [shape=point, width=0.05, height=0.05]
 {MARSTA, BMI}-> INTER4 [style = bold, dir=none, style = dashed]
 INTER4       -> EQ5D   [style = bold, arrowsize = 0.7, style = dashed]
-BMI          -> EQ5D   [style = bold, arrowsize = 0.7]
-
 }")
 
 all  %>% 
